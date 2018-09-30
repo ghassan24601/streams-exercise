cd451926886e85c1a07dbfb50f11cce615c4a1fd
Ghassan Al Mamar <Ghassan.Al.Mamar@oracle.com>
2018-09-29 18:00:46 -0700

    Streams assignment

--- stream_exercise.py
+++ stream_exercise.py
@@ -47,7 +47,6 @@ class StreamProcessor(object):
 
     def process(self):
         """
-        TODO: Implement the `process` method, as described above.
         
         :return: int
         """
@@ -56,12 +55,15 @@ class StreamProcessor(object):
                    # together.
         total = 0  # The running total of sums.
 
-        # TODO: WRITE CODE HERE:
+        while count < 10 and total < 200:
+            digits = self._stream.read(2)
+            if len(digits) < 2:
+                break
 
-        # Just some example syntax, you can read two digits from the head of the
-        # stream using the following code:
-        #
-        # digits = self._stream.read(2)
+            count += 1
 
+            number = int(digits)
+            total += number
 
         return count
+
